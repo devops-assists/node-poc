fe02e9fae9254898e933e01f079d9d05585d29db
soban <sobankhan252@gmail.com>
2024-08-17 11:03:54 +0500

    add

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route at /node-poc
+app.get('/node-poc', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
